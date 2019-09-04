d772fb2260f554a09a170cd13c117513e9ef3971
jaecheon <jaecheon0222@gmail.com>
2019-09-04 20:40:49 +0900

    add post profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -33,6 +33,46 @@ router.get('/', checkAuth, (req, res) => {
  * @access  Private
  */
 router.post('/', checkAuth, (req, res) => {
+    // Get fields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubusername) profileFields.githubusername = req.body.githubusername;
+
+    //Skills - Spilt into array
+    if (typeof req.body.skills !== 'undefined') {
+        profileFields.skills = req.body.skills.split(',');
+    }
+
+    // Social
+    profileFields.social = {};
+
+    profileModel
+        .findOne({user: req.user.id})
+        .then(profile => {
+            profileModel
+                .findOne({handle: profileFields.handle})
+                .then(profile => {
+                    if (profile) {
+                        errors.handle = 'That handle already exists';
+                        res.status(400).json(errors);
+                    }
+                    new profileModel(profileFields)
+                        .save()
+                        .then(profile => {
+                            res.json(profile)
+                        })
+                        .catch(err => res.json(err));
+
+                });
+        })
+        .catch(err => res.json(err));
 
 });
 
