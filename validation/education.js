57e1fa9e0c76b27b1441b7821d0d6fa850f50886
jaecheon <jaecheon0222@gmail.com>
2019-09-11 15:37:50 +0900

    add validator (experice, education)

---
+++ validation/education.js
@@ -0,0 +1,53 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validateEducationInput(data) {
+
+    let errors = {};
+
+    // 사용자 입력 값 체크
+    data.school = !isEmpty(data.school) ? data.school : '';
+    data.degree = !isEmpty(data.degree) ? data.degree : '';
+    data.fieldofstudy = !isEmpty(data.fieldofstudy) ? data.fieldofstudy : '';
+    data.from = !isEmpty(data.from) ? data.from : '';
+
+    if (Validator.isEmpty(data.school)) {
+        errors.school = 'Profile school is required'
+    }
+
+    if (Validator.isEmpty(data.degree)) {
+        errors.degree = 'Profile degree is required'
+    }
+
+    if (Validator.isEmpty(data.fieldofstudy)) {
+        errors.fieldofstudy = 'Profile fieldofstudy'
+    }
+
+    if (Validator.isEmpty(data.from)) {
+        errors.from = 'Profile from'
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+
+
+    // school: {
+    //     type: String,
+    //     required: true
+    // },
+    // degree: {
+    //     type: String,
+    //     required: true
+    // },
+    // fieldofstudy: {
+    //     type: String,
+    //     required: true
+    // },
+    // from: {
+    //     type: Date,
+    //     required: true
+    // },
+
+}
---
+++ validation/experience.js
@@ -0,0 +1,49 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validateExperienceInput(data) {
+
+    let errors = {};
+
+    data.title = !isEmpty(data.title) ? data.title : '';
+    data.company = !isEmpty(data.company) ? data.company : '';
+    data.location = !isEmpty(data.location) ? data.location : '';
+    data.from = !isEmpty(data.from) ? data.from : '';
+
+    if (Validator.isEmpty(data.title)) {
+        errors.title = 'Profile title is required'
+    }
+
+    if (Validator.isEmpty(data.company)) {
+        errors.company = 'Profile company is required'
+    }
+
+    if (Validator.isEmpty(data.location)) {
+        errors.location = 'Profile location is required'
+    }
+    
+    if (Validator.isEmpty(data.form)) {
+        errors.from = 'Profile form is required'
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+
+    // title: {
+    //     type: String,
+    //     required: true
+    // },
+    // company: {
+    //     type: String,
+    //     required: true
+    // },
+    // location: {
+    //     type: String
+    // },
+    // from: {
+    //     type: Date,
+    //     required: true
+    // },
+}
